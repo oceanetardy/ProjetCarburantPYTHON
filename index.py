835c530470687a4e6e2764ca5a6dbcd8daf50003
Océane TARDY <tardy.oceane42@gmail.com>
2024-01-26 18:44:19 +0100

    [*] On oublie l'API + remise du cache

--- index.py
+++ index.py
@@ -3,7 +3,6 @@ import json
 import requests
 from datetime import datetime, timedelta
 import pytz
-import time
 
 CACHE_FOLDER = "cache"  # Dossier où les fichiers de cache seront stockés
 CACHE_EXPIRATION = timedelta(hours=1)  # Durée d'expiration du cache
@@ -18,12 +17,10 @@ def charger_donnees_json_de_url(url):
         timestamp_expiration = os.path.getmtime(cacheFichier) + CACHE_EXPIRATION.total_seconds()
         current_timestamp = datetime.now().timestamp()
 
-        expiration_date = datetime.fromtimestamp(timestamp_expiration, tz=pytz.timezone("Europe/Paris")).strftime(
-            '%Y-%m-%d %H:%M:%S %Z')
+        expiration_date = datetime.fromtimestamp(timestamp_expiration, tz=pytz.timezone("Europe/Paris")).strftime('%Y-%m-%d %H:%M:%S %Z')
         print(f"Date d'expiration : {expiration_date}")
 
-        current_date = datetime.fromtimestamp(current_timestamp, tz=pytz.timezone("Europe/Paris")).strftime(
-            '%Y-%m-%d %H:%M:%S %Z')
+        current_date = datetime.fromtimestamp(current_timestamp, tz=pytz.timezone("Europe/Paris")).strftime('%Y-%m-%d %H:%M:%S %Z')
         print(f"Heure actuelle : {current_date}")
 
         if current_timestamp < timestamp_expiration:
@@ -41,25 +38,15 @@ def charger_donnees_json_de_url(url):
 
     if reponse.status_code == 200:
         donnees_json = reponse.json()
+        print(f"Données chargées depuis {url}, {len(donnees_json)} éléments")
 
-        # Vérifier si les données sont une liste
-        if isinstance(donnees_json, list):
-            # Pour chaque record dans les données JSON, ajouter le nom de la marque
-            for record in donnees_json:
-                station_id = record.get('id', None)
-                if station_id:
-                    brand_name = get_station_info_by_id(station_id)
-                    record['brand_name'] = brand_name
+        # Sauvegarder les données dans le cache
+        os.makedirs(CACHE_FOLDER, exist_ok=True)
+        with open(cacheFichier, 'w', encoding='utf-8') as fichier_cache:
+            json.dump(donnees_json, fichier_cache, ensure_ascii=False)
+            print(f"Données sauvegardées dans le cache {cacheFichier}, {len(donnees_json)} éléments")
 
-            # Sauvegarder les données dans le cache
-            os.makedirs(CACHE_FOLDER, exist_ok=True)
-            with open(cacheFichier, 'w', encoding='utf-8') as fichier_cache:
-                json.dump(donnees_json, fichier_cache, ensure_ascii=False)
-                print(f"Données sauvegardées dans le cache {cacheFichier}, {len(donnees_json)} éléments")
-
-            return donnees_json
-        else:
-            print("Les données ne sont pas sous la forme attendue.")
+        return donnees_json
     else:
         print(f"Échec du chargement des données depuis {url}. Code d'état : {reponse.status_code}")
         return None
@@ -69,45 +56,9 @@ def sauvegarder_donnees_json(nom_fichier, donnees_json):
         json.dump(donnees_json, fichier, ensure_ascii=False)
         print(f"Données sauvegardées dans {nom_fichier}, {len(donnees_json)} éléments")
 
-
-def get_station_info_by_id(station_id):
-    url = f"https://api.prix-carburants.2aaz.fr/station/{station_id}"
-
-    try:
-        response = requests.get(url)
-
-        # Vérifier si la requête a réussi
-        if response.status_code == 200:
-            # Vérifier si la réponse contient des données JSON
-            if response.text.strip():  # Si la réponse n'est pas vide
-                data = response.json()
-
-                # Récupérer le nom de la marque associée
-                brand_info = data.get('Brand', {})
-                brand_name = brand_info.get('name', 'Nom de la marque non disponible')
-                return brand_name
-            else:
-                print(f"Aucune donnée JSON trouvée dans la réponse pour la station id : {str(station_id)}")
-        elif response.status_code == 429:
-            # Gérer l'erreur 429 en attendant le temps spécifié par l'en-tête Retry-After
-            retry_after = int(response.headers.get('Retry-After', 30))  # Attendre par défaut 30 secondes
-            print(f"Erreur 429 - Trop de requêtes. Attente de {retry_after} secondes., Station id : {str(station_id)} ")
-            time.sleep(retry_after)
-            return get_station_info_by_id(station_id)  # Réessayer après l'attente
-        else:
-            print(f"Erreur lors de la requête : {response.status_code} ,Station id : {str(station_id)}")
-    except Exception as e:
-        print(f"Erreur lors de la requête : {str(e)}, Station id : {str(station_id)}")
-
-# Charger les données JSON depuis l'URL
+# Données json
 url = "https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/exports/json?lang=fr&timezone=Europe%2FParis"
-# Charger les données JSON depuis l'URL 10 fois avec un intervalle de 10 secondes
-for _ in range(5):
-    donnees_json_de_url = charger_donnees_json_de_url(url)
+donnees_json_de_url = charger_donnees_json_de_url(url)
 
-    # Sauvegarder les données mises à jour dans un nouveau fichier
-    if donnees_json_de_url:
-        sauvegarder_donnees_json("sortie.json", donnees_json_de_url)
-
-    # Ajouter une pause de 10 secondes entre chaque appel
-    time.sleep(10)
+if donnees_json_de_url:
+    sauvegarder_donnees_json("sortie.json", donnees_json_de_url)
