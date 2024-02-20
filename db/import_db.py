d895333492b9554ea654fef29d239dc263cd6ba9
Océane TARDY <tardy.oceane42@gmail.com>
2024-02-20 15:01:49 +0100

    [+] WIP: Enregistrement en BDD changement fichier db

--- db/import_db.py
+++ db/import_db.py
@@ -55,32 +55,45 @@ def create_tables():
     conn.close()
 
 # Fonction pour insérer les données JSON d'une station dans la base de données
+# Fonction pour insérer les données JSON d'une station dans la base de données
 def insert_data_from_json(station_data):
-    conn = sqlite3.connect('stations_data.db')
+    conn = sqlite3.connect('db/stations_data.db')
     cursor = conn.cursor()
 
-    # Vérifier si la station existe déjà dans la base de données
-    cursor.execute("SELECT id FROM Station WHERE id = ?", (station_data['id'],))
-    existing_station = cursor.fetchone()
-    if existing_station:
-        print(f"La station avec l'id {station_data['id']} existe déjà dans la base de données. Ignorer l'insertion.")
+    try:
+        # Insérer les données de la station
+        station_values = (station_data['id'], station_data.get('nom', ''), station_data.get('adresse', ''),
+                          station_data.get('latitude', 0), station_data.get('longitude', 0),
+                          station_data.get('cp', ''), station_data.get('ville', ''),
+                          station_data.get('departement', ''), station_data.get('code_departement', ''),
+                          station_data.get('region', ''), station_data.get('code_region', ''),
+                          station_data.get('marque', ''), station_data.get('horaires_automate_24_24', ''))
+        cursor.execute('''INSERT OR IGNORE INTO Station (id, nom, adresse, latitude, longitude, cp, ville, 
+                          departement, code_departement, region, code_region, marque, horaires_automate_24_24) 
+                          VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', station_values)
+
+        # Insérer les services de la station
+        for service in station_data.get('services', {}).get('service', []):
+            cursor.execute('''INSERT OR IGNORE INTO Service (nom) VALUES (?)''', (service,))
+            service_id = cursor.lastrowid
+            cursor.execute('''INSERT OR IGNORE INTO Station_Service (station_id, service_id) VALUES (?, ?)''',
+                           (station_data['id'], service_id))
+
+        # Insérer les prix des carburants
+        for prix in station_data.get('prix', []):
+            cursor.execute('''INSERT OR IGNORE INTO Carburant (nom) VALUES (?)''', (prix['@nom'],))
+            carburant_id = cursor.lastrowid
+            prix_values = (station_data['id'], carburant_id, prix['@valeur'], prix['@maj'])
+            cursor.execute('''INSERT OR IGNORE INTO Prix (station_id, carburant_id, prix, maj) VALUES (?, ?, ?, ?)''',
+                           prix_values)
+
+        conn.commit()
+        print(f"Données pour la station avec l'ID {station_data['id']} insérées avec succès.")
+    except Exception as e:
+        print(f"Erreur lors de l'insertion des données pour la station avec l'ID {station_data['id']}: {str(e)}")
+    finally:
         conn.close()
-        return
 
-    # Insérer les données de la station
-    station_values = (station_data['id'], station_data['nom'], station_data.get('adresse', ''),
-                      station_data.get('latitude', 0), station_data.get('longitude', 0),
-                      station_data.get('cp', ''), station_data.get('ville', ''),
-                      station_data.get('departement', ''), station_data.get('code_departement', ''),
-                      station_data.get('region', ''), station_data.get('code_region', ''),
-                      station_data.get('marque', ''), station_data.get('horaires_automate_24_24', ''))
-    cursor.execute('''INSERT INTO Station (id, nom, adresse, latitude, longitude, cp, ville, departement, code_departement, 
-                      region, code_region, marque, horaires_automate_24_24) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', station_values)
-
-    # Insérer les autres données (services, prix, etc.)
-
-    conn.commit()
-    conn.close()
 
 
 # Charger les données JSON depuis un fichier
