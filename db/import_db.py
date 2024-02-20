9ddec1b8bc3af83c8b8eac313f364876f6f2858e
Océane TARDY <tardy.oceane42@gmail.com>
2024-02-20 14:40:03 +0100

    [+] WIP: Enregistrement en BDD

--- db/import_db.py
+++ db/import_db.py
@@ -1,80 +1,97 @@
 import sqlite3
 import json
 
-file_encoding = 'utf-8'
+# Fonction pour créer la base de données et les tables
+def create_tables():
+    conn = sqlite3.connect('db/stations_data.db')
+    cursor = conn.cursor()
+
+    # Créer les tables nécessaires si elles n'existent pas déjà
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Station (
+                        id INTEGER PRIMARY KEY,
+                        nom TEXT,
+                        adresse TEXT,
+                        latitude REAL,
+                        longitude REAL,
+                        cp TEXT,
+                        ville TEXT,
+                        departement TEXT,
+                        code_departement TEXT,
+                        region TEXT,
+                        code_region TEXT,
+                        marque TEXT,
+                        horaires_automate_24_24 TEXT
+                    )''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Service (
+                        id INTEGER PRIMARY KEY,
+                        nom TEXT
+                    )''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Station_Service (
+                        station_id INTEGER,
+                        service_id INTEGER,
+                        PRIMARY KEY (station_id, service_id),
+                        FOREIGN KEY (station_id) REFERENCES Station(id),
+                        FOREIGN KEY (service_id) REFERENCES Service(id)
+                    )''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Carburant (
+                        id INTEGER PRIMARY KEY,
+                        nom TEXT
+                    )''')
+
+    cursor.execute('''CREATE TABLE IF NOT EXISTS Prix (
+                        id INTEGER PRIMARY KEY,
+                        station_id INTEGER,
+                        carburant_id INTEGER,
+                        prix REAL,
+                        maj TEXT,
+                        FOREIGN KEY (station_id) REFERENCES Station(id),
+                        FOREIGN KEY (carburant_id) REFERENCES Carburant(id)
+                    )''')
+
+    conn.commit()
+    conn.close()
+
+# Fonction pour insérer les données JSON d'une station dans la base de données
+def insert_data_from_json(station_data):
+    conn = sqlite3.connect('stations_data.db')
+    cursor = conn.cursor()
+
+    # Vérifier si la station existe déjà dans la base de données
+    cursor.execute("SELECT id FROM Station WHERE id = ?", (station_data['id'],))
+    existing_station = cursor.fetchone()
+    if existing_station:
+        print(f"La station avec l'id {station_data['id']} existe déjà dans la base de données. Ignorer l'insertion.")
+        conn.close()
+        return
+
+    # Insérer les données de la station
+    station_values = (station_data['id'], station_data['nom'], station_data.get('adresse', ''),
+                      station_data.get('latitude', 0), station_data.get('longitude', 0),
+                      station_data.get('cp', ''), station_data.get('ville', ''),
+                      station_data.get('departement', ''), station_data.get('code_departement', ''),
+                      station_data.get('region', ''), station_data.get('code_region', ''),
+                      station_data.get('marque', ''), station_data.get('horaires_automate_24_24', ''))
+    cursor.execute('''INSERT INTO Station (id, nom, adresse, latitude, longitude, cp, ville, departement, code_departement, 
+                      region, code_region, marque, horaires_automate_24_24) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', station_values)
+
+    # Insérer les autres données (services, prix, etc.)
+
+    conn.commit()
+    conn.close()
 
 
-with open('cache/cache_file.json', 'r', encoding=file_encoding) as file:
-    data_list = json.load(file)
+# Charger les données JSON depuis un fichier
+with open('cache/cache_file.json', 'r', encoding='utf-8') as file:
+    json_data_list = json.load(file)
 
+# Créer les tables dans la base de données
+create_tables()
 
-# Établir une connexion à la base de données (crée le fichier si inexistant)
-conn = sqlite3.connect('db/stations_data.db')
+# Insérer les données JSON de chaque station dans la base de données
+for station_data in json_data_list:
+    insert_data_from_json(station_data)
 
-# Créer un curseur pour exécuter des requêtes SQL
-cursor = conn.cursor()
-
-# Créer la table pour stocker les données si elle n'existe pas déjà
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS stations (
-        id INTEGER PRIMARY KEY,
-        latitude TEXT,
-        longitude REAL,
-        cp TEXT,
-        pop TEXT,
-        adresse TEXT,
-        ville TEXT,
-        horaires TEXT,
-        services TEXT,
-        prix TEXT,
-        geom TEXT,
-        gazole_maj TEXT,
-        gazole_prix TEXT,
-        sp95_maj TEXT,
-        sp95_prix TEXT,
-        e85_maj TEXT,
-        e85_prix TEXT,
-        gplc_maj TEXT,
-        gplc_prix TEXT,
-        e10_maj TEXT,
-        e10_prix TEXT,
-        sp98_maj TEXT,
-        sp98_prix TEXT,
-        carburants_disponibles TEXT,
-        carburants_indisponibles TEXT,
-        horaires_automate_24_24 TEXT,
-        services_service TEXT,
-        departement TEXT,
-        code_departement TEXT,
-        region TEXT,
-        code_region TEXT,
-        marque TEXT,
-        nom TEXT
-    )
-''')
-
-# Insérer chaque enregistrement dans la table
-for data in data_list:
-
-    horaires_str = json.dumps(data['horaires'])
-    print(f"{horaires_str}")
-
-    services_str = json.dumps(data['services'])
-    prix_str = json.dumps(data['prix'])
-    geom_str = json.dumps(data['geom'])
-    carburants_disponibles_str = json.dumps(data['carburants_disponibles'])
-    carburants_indisponibles_str = json.dumps(data['carburants_indisponibles'])
-    services_service_str = json.dumps(data['services_service'])
-
-    cursor.execute('''
-        INSERT INTO stations VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    ''', (data['id'], data['latitude'], data['longitude'], data['cp'], data['pop'], data['adresse'], data['ville'],
-          horaires_str, services_str, prix_str, geom_str, data['gazole_maj'], data['gazole_prix'], data['sp95_maj'],
-          data['sp95_prix'], data['e85_maj'], data['e85_prix'], data['gplc_maj'], data['gplc_prix'], data['e10_maj'],
-          data['e10_prix'], data['sp98_maj'], data['sp98_prix'], carburants_disponibles_str, carburants_indisponibles_str,
-          data['horaires_automate_24_24'], services_service_str, data['departement'], data['code_departement'],
-          data['region'], data['code_region'], data['marque'], data['nom']))
-
-# Valider les modifications et fermer la connexion
-conn.commit()
-conn.close()
+print("Les données ont été insérées avec succès dans la base de données.")
