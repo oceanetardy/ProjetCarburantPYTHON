378edcca01add26e26d8514a1f47f165ffbd0299
Océane TARDY <tardy.oceane42@gmail.com>
2024-03-29 11:04:41 +0100

    [*] Docker correction

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	return render_template('templateHome.html', content="Hello World!")
+	return render_template('templateHome.html', content="Hello World!", test='test2')
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000, debug=True)
