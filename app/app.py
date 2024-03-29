99596a1ddd432d7cb78f4070ed47dae5d1ea01d2
Océane TARDY <tardy.oceane42@gmail.com>
2024-03-29 18:17:44 +0100

    [*] FO : Mise en place dossier IMG flask + logo

--- app/app.py
+++ app/app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+IMG_FOLDER = os.path.join("static", "IMG")
+app.config["IMG_FOLDER"] = IMG_FOLDER  # A vérifier
+
 
 @app.route('/')
 def hello():
-	return render_template('templateHome.html')
+	logoGasGenius = os.path.join(app.config["IMG_FOLDER"], "logoGasGenius.png")
+	return render_template('templateHome.html', logo=logoGasGenius)
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
