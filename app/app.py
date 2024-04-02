642b9e87451c79891cc37a205609224151c55742
Océane TARDY <tardy.oceane42@gmail.com>
2024-04-02 13:33:30 +0200

    [+] Mise en place des routes et différentes pages

--- app/app.py
+++ app/app.py
@@ -4,13 +4,20 @@ import os
 app = Flask(__name__)
 IMG_FOLDER = os.path.join("static", "IMG")
 app.config["IMG_FOLDER"] = IMG_FOLDER
-
+logoGasGenius = os.path.join(app.config["IMG_FOLDER"], "logoGasGenius.png")
+gasstation = os.path.join(app.config["IMG_FOLDER"], "gasstation.jpg")
 
 @app.route('/')
 def home():
-	logoGasGenius = os.path.join(app.config["IMG_FOLDER"], "logoGasGenius.png")
-	gasstation = os.path.join(app.config["IMG_FOLDER"], "gasstation.jpg")
 	return render_template('home.html', logo=logoGasGenius, gasstation=gasstation)
+
+@app.route('/statistiques-nationales')
+def statisticNational():
+	return render_template('nationalstatistic.html', logo=logoGasGenius, gasstation=gasstation)
+
+@app.route('/rechercher')
+def findstation():
+	return render_template('findstation.html', logo=logoGasGenius, gasstation=gasstation)
 
 
 if __name__ == '__main__':
