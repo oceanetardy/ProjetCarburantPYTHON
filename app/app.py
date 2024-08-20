10c41ca98d3589db2ea413e7774594f401cc6a51
Océane TARDY <tardy.oceane42@gmail.com>
2024-08-20 18:55:32 +0200

    ✨ Ajout moteur de recherche
    Moteur de recherche par code postal

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
-from db.request_sql import get_carburant_info, generate_carburant_plot
+from db.request_sql import get_carburant_info, generate_carburant_plot, search_stations_by_department, \
+    get_carburant_prices_by_postal_code
 
 app = Flask(__name__)
 IMG_FOLDER = os.path.join("static", "IMG")
@@ -10,12 +11,11 @@ app.config["PLOT_FOLDER"] = PLOT_FOLDER
 logoGasGenius = os.path.join(app.config["IMG_FOLDER"], "logoGasGenius.png")
 gasstation = os.path.join(app.config["IMG_FOLDER"], "gasstation.jpg")
 gaspump = os.path.join(app.config["IMG_FOLDER"], "gaspump.jpg")
-# Image pour visuel seulement matplotlib -> Sera géré en Python
 feinte = os.path.join(app.config["IMG_FOLDER"], "feinte.png")
 
 @app.route('/')
 def home():
-	return render_template('home.html', logo=logoGasGenius, gasstation=gasstation)
+    return render_template('home.html', logo=logoGasGenius, gasstation=gasstation)
 
 @app.route('/statistiques-nationales')
 def statisticNational():
@@ -23,10 +23,23 @@ def statisticNational():
     carburant_info = get_carburant_info(db_path)
     plot_path = generate_carburant_plot(carburant_info, app.config["PLOT_FOLDER"])
     return render_template('nationalstatistic.html', logo=logoGasGenius, gasstation=gasstation, gaspump=gaspump, carburant_info=carburant_info, plot_path=plot_path)
-@app.route('/rechercher')
-def findstation():
-	return render_template('findstation.html', logo=logoGasGenius, gasstation=gasstation)
 
+@app.route('/rechercher', methods=['GET', 'POST'])
+def findstation():
+    db_path = 'db/stations_data.db'
+    if request.method == 'POST':
+        postal_code = request.form.get('department')
+        if postal_code:
+            stations = search_stations_by_department(db_path, postal_code)
+            prices = get_carburant_prices_by_postal_code(db_path, postal_code)
+        else:
+            stations = []
+            prices = []
+    else:
+        stations = []
+        prices = []
+
+    return render_template('findstation.html', logo=logoGasGenius, gasstation=gasstation, stations=stations, prices=prices)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
--- app/db/request_sql.py
+++ app/db/request_sql.py
@@ -75,3 +75,49 @@ def generate_carburant_plot(carburant_info, output_folder):
     plt.close()
 
     return plot_path
+
+
+def search_stations_by_department(db_path, department_code):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    query = '''
+        SELECT s.nom, s.adresse, s.ville, s.cp
+        FROM Station s
+        WHERE s.cp LIKE ?
+    '''
+    cursor.execute(query, (department_code + '%',))
+    stations = cursor.fetchall()
+
+    conn.close()
+    return stations
+
+def get_carburant_prices_by_postal_code(db_path, postal_code):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # Define a query that fetches the prices for each fuel type
+    query = '''
+        SELECT 
+            COALESCE(s.nom, 'Station non disponible') AS station_nom, 
+            COALESCE(s.adresse, 'Adresse non disponible') AS adresse, 
+            COALESCE(s.ville, 'Ville non disponible') AS ville, 
+            COALESCE(s.cp, 'CP non disponible') AS cp,
+            COALESCE(MAX(CASE WHEN c.nom = 'Gazole' THEN p.prix ELSE NULL END), 'Carburant non disponible') AS Gazole,
+            COALESCE(MAX(CASE WHEN c.nom = 'E10' THEN p.prix ELSE NULL END), 'Carburant non disponible') AS E10,
+            COALESCE(MAX(CASE WHEN c.nom = 'SP98' THEN p.prix ELSE NULL END), 'Carburant non disponible') AS SP98,
+            COALESCE(MAX(CASE WHEN c.nom = 'SP95' THEN p.prix ELSE NULL END), 'Carburant non disponible') AS SP95,
+            COALESCE(MAX(CASE WHEN c.nom = 'E85' THEN p.prix ELSE NULL END), 'Carburant non disponible') AS E85,
+            COALESCE(MAX(CASE WHEN c.nom = 'GPLc' THEN p.prix ELSE NULL END), 'Carburant non disponible') AS GPLc
+        FROM Station s
+        LEFT JOIN Prix p ON s.id = p.station_id
+        LEFT JOIN Carburant c ON p.carburant_id = c.id
+        WHERE s.cp = ?
+        GROUP BY s.nom, s.adresse, s.ville, s.cp
+    '''
+
+    cursor.execute(query, (postal_code,))
+    prices = cursor.fetchall()
+
+    conn.close()
+    return prices
